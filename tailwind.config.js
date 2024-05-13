1dcb05da825ed5d20b91baf5b7c4b43a3c806b60
Moth <kampanee.moth@gmail.com>
2024-05-13 10:29:57 +0700

    style: add script color and add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1f1e61",
+        second: "#1374f6",
+        third: "#fdc46e",
+        forth: "#0b0757",
+        p: "#8480ae",
+        bg: "#f4f7fc",
+      },
+    },
+  },
+};
